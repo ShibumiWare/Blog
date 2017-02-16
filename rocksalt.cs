07821d034dc9a47490de18a0b9bc3295a52453a0
ShibumiWare <shibumi@shibumiware.com>
2017-02-16 15:50:31 -0600

    Create rocksalt.cs

---
+++ rocksalt.cs
@@ -0,0 +1,10 @@
+/* rock-salt-regular - latin */
+@font-face {
+    font-family: 'Rock Salt';
+    font-style: normal;
+    font-weight: 400;    
+    local('../Rock Salt'), local('RockSalt'), url('rock-salt-v6-latin-regular.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+    url('../rock-salt-v6-latin-regular.woff2') format('woff2'), /* Super Modern Browsers */
+    url('../rock-salt-v6-latin-regular.woff') format('woff'), /* Modern Browsers */
+    url('../rock-salt-v6-latin-regular.ttf') format('truetype'), /* Safari, Android, iOS */
+    url('../rock-salt-v6-latin-regular.svg#RockSalt') format('svg'); /* Legacy iOS */
