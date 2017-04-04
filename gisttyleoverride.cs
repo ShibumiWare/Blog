97be3f445fbd155b8e3246a992e7fbfb74511177
ShibumiWare <shibumi@shibumiware.com>
2017-04-04 18:27:11 -0500

    Create gisttyleoverride.cs

---
+++ gisttyleoverride.cs
@@ -0,0 +1,73 @@
+
+    .gist {
+        color: #000000;
+        direction: ltr;
+        font-size: 14px;
+        text-align: left;
+    }
+
+    .gist-meta a {
+        color: #000000 !important;
+        text-decoration: none;
+    }
+
+    .gist .gist .blob-wrapper table {
+        border-collapse: collapse;
+    }
+
+    .gist .highlight {
+        background: #fff;
+        border: 0;
+        color: #333;
+        font-family: Consolas, "Liberation Mono", Menlo, Courier, monospace;
+        font-size: 12px;
+        font-weight: normal;
+        line-height: 1.4;
+        margin: 0;
+        padding: 0;
+    }
+
+    .gist .gist-file {
+        border: 1px solid #ddd;
+        border-bottom: 1px solid #ccc;
+        border-radius: 3px;
+        font-family: Consolas, "Liberation Mono", Menlo, Courier, monospace;
+        margin-bottom: 1em;
+    }
+
+    .gist .gist-data {
+        background-color: pink;
+        border-bottom: 1px solid #ddd;
+        border-radius: 2px 2px 0 0;
+        overflow: auto;
+        word-wrap: normal;
+    }
+
+    .gist .blob-code {
+        background: #d3d3d3;
+        padding: 1px 10px !important;
+        text-align: left;
+    }
+
+    .gist .blob-num {
+        background: #dcdcdc;
+        min-width: inherit;
+        padding: 1px 10px !important;
+    }
+
+    .gist .gist-meta {
+        background-color: #767676;
+        border-radius: 0 0 2px 2px;
+        color: #f5f5f5;
+        font: 12px -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+        overflow: hidden;
+        padding: 10px;
+    }
+
+    .gist .gist-data {
+        background-color: #fff;
+        border-bottom: 1px solid #ddd;
+        border-radius: 2px 2px 0 0;
+        overflow: auto;
+        word-wrap: normal;
+    }
